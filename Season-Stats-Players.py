4acba886f27320bb0882538cdf052e6b24b6efcb
Dylan Drumm <dyl.drumm@gmail.com>
2022-11-18 16:33:36 -0500

    Trimmed Scraper, Generated 2020-22 Seasons
    Updated Player Stats Scraper, generated 2020-2022 season player stats

--- Season-Stats-Players.py
+++ Season-Stats-Players.py
@@ -3,30 +3,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 season = 2022
-url = "https://www.pro-football-reference.com/years/{}/fantasy.htm".format(season)
-html = urlopen(url)
-soup = BeautifulSoup(html, "html.parser")
 
-headers = [th.getText() for th in soup.findAll('tr')[1].findAll('th')]
-headers = headers[1:]
-print(headers)
-
-rows = soup.findAll('tr', class_=lambda table_rows: table_rows != "thead")
-player_stats = [[td.getText() for td in rows[i].findAll('td')]
-                for i in range(len(rows))]
-print(player_stats[2])
-
-stats = pd.DataFrame(player_stats, columns = headers)
-stats.head()
-stats = stats.replace(r'', 0, regex=True)
-stats['Year'] = season
-stats.head()
-
-
-def player_csv(year):
-    url = "https://www.pro-football-reference.com/years/{}/fantasy.htm".format(year)
+def player_csv(season):
+    url = "https://www.pro-football-reference.com/years/{}/fantasy.htm".format(season)
     html = urlopen(url)
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
 
     headers = [th.getText() for th in soup.findAll('tr')[1].findAll(
         'th')]
@@ -41,8 +22,10 @@ def player_csv(year):
     stats = pd.DataFrame(player_stats, columns=headers)
 
     stats = stats.replace(r'', 'N/A', regex=True)
-    stats['Year'] = year
+    stats['Season'] = season
 
-    stats.to_csv('{}playerstats.csv'.format(year))
+    stats.to_csv('Football-CSVs/{}playerstats.csv'.format(season))
 
-    print("Player stats for  {} season created.".format(year))
+    print("Player stats for  {} season created.".format(season))
+
+player_csv(season)
