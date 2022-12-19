b555267bbfe86f8a82bac526e77b0d0cd12003c1
Dylan Drumm <dyl.drumm@gmail.com>
2022-12-19 17:51:41 -0500

    CSV Display function
    Load and display locally stored CSVs (only up to 30 entries currently, graphical issue with Panel storing entries.  Will make scrollable to fix.)

--- GUI/GUIhelper.py
+++ GUI/GUIhelper.py
@@ -1,4 +1,5 @@
 from functools import partial
+from itertools import islice
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
@@ -31,39 +32,66 @@ def generate_csv_manager(root, sport):
     return panel
 
 # Generate Label, Field and Button to enter season and load stats
-def generate_season_input_field(root):    
+def generate_season_input_field(root):
     season = tk.StringVar()
-    
+
     panel = PanedWindow(root, bd = 1, relief = "raised", bg = "grey", orient = VERTICAL)
-    
+
     seasonInputLabel = tk.Label(panel, text = "Enter the season you want to view:")
     seasonInputField = tk.Entry(panel, text = "black", bg = "lightgrey", width = 15, textvariable = season)
     seasonInputButton = tk.Button(panel, width = 12, relief = RAISED, text = "Get season stats")
-    
+
     seasonInputLabel.pack(side = TOP)
     seasonInputField.pack(side = TOP)
     seasonInputButton.pack(side = TOP)
-    
+
     return panel
-    
+
 # Generate table containing stats for selected season (Generated from stored CSV)
-def display_csv(root, season):   
-    #panel = PanedWindow(root, bd = 1, relief = "sunken", bg = "grey", orient = VERTICAL) 
-    fileName = season + "footballplayerstats.csv"
-    print(fileName)
+def display_csv(root, season):
+    panel = PanedWindow(root, bd = 1, relief = "sunken", bg = "grey", orient = VERTICAL, borderwidth = 1)
+    fileName = "CSVs/football/players/" + season + "footballplayerstats.csv"
+    headers = list()
+    headers.append("Rank")
+
+    with open(fileName, newline = '') as file:
+        reader = csv.reader(file)
+        first = next(reader)
+        for head in first:
+            if(head != ''):
+                headers.append(head)
+
+        labels = PanedWindow(panel, bd = 1, relief = "sunken", bg = "grey", orient = VERTICAL, borderwidth = 1)
     
-    margin = PanedWindow(root)
-    scrollbarX = Scrollbar(margin, orient = HORIZONTAL)
-    scrollbarY = Scrollbar(margin, orient = VERTICAL)
-    tree = ttk.Treeview(margin, selectmode = "extended", yscrollcommand = scrollbarY.set, xscrollcommand = scrollbarX.set)
+        for i, head in enumerate(headers, start = 0):
+            label = tk.Label(labels, text = head, height = 2, width = 13, font=("Arial", 8))
+            label.pack(side = LEFT)
+
+        values = PanedWindow(panel, bd = 1, relief = "sunken", bg = "grey", orient = VERTICAL, borderwidth = 1)
+        data = list(reader)
+        
+        for entry in islice(data, 30):
+            temp = PanedWindow(panel, bd = 1, relief = "sunken", bg = "grey", orient = HORIZONTAL, borderwidth = 1)
+            for val in entry:
+                label = tk.Label(temp, text = val, height = 1, width = 13, font=("Arial", 8))
+                label.pack(side = LEFT)
+            values.add(temp)
+
+    labels.pack()
+    values.pack(fill = tk.BOTH)
+    root.add(panel)
+    return panel
+
+    #margin = PanedWindow(root)
+    #scrollbarX = Scrollbar(margin, orient = HORIZONTAL)
+    #scrollbarY = Scrollbar(margin, orient = VERTICAL)
+    #tree = ttk.Treeview(margin, selectmode = "extended", yscrollcommand = scrollbarY.set, xscrollcommand = scrollbarX.set)
     
-    scrollbarX.config(command = tree.xview)
-    scrollbarY.config(comman = tree.yview)
+    #scrollbarX.config(command = tree.xview)
+    #scrollbarY.config(comman = tree.yview)
     
-    scrollbarX.pack(side = BOTTOM, fill = X)
-    scrollbarY.pack(side = RIGHT, fill = Y)
+    #scrollbarX.pack(side = BOTTOM, fill = X)
+    #scrollbarY.pack(side = RIGHT, fill = Y)
     
-    headTitles = list()
-    playerValues = list()
-    
-    return
+    #headTitles = list()
+    #playerValues = list()
