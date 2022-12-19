a5ee5b8dd4cb7394d51703f4558a7e19b0a64377
Dylan Drumm <dyl.drumm@gmail.com>
2022-12-19 16:32:28 -0500

    Added CSV manager bar
    Added GUI helper functionality to generate a panel with all available locally stored CSVs

--- GUI/GUIhelper.py
+++ GUI/GUIhelper.py
@@ -1,15 +1,32 @@
+from functools import partial
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 from tkinter import filedialog
 import csv
+import os
 
 localSeason = (str)
 
 # Generate default tab template
 def load_tab_base(root):
-    panel = PanedWindow(root, bd = 1, relief = "sunken", bg = "lightgrey")
-    panel.pack(expand = True, fill = tk.BOTH)
+    panel = PanedWindow(root, bd = 1, relief = "sunken", bg = "lightgrey", orient = HORIZONTAL)
+    
+    return panel
+
+def generate_csv_manager(root, sport):
+    panel = PanedWindow(root, bd = 1, relief = "raised", bg = "lightgrey", orient = VERTICAL)
+    
+    # Get list of already generated seasons
+    seasons = list()
+    direct = os.fsencode("CSVs/football/players/")
+    for file in os.listdir(direct):
+        filename = os.fsdecode(file)
+        seasons.append(filename[0:4])
+    # Generate button for each season
+    for season in seasons:
+        tempButton = tk.Button(panel, width = 8, relief = RAISED, text = season + " season player stats", command = partial(display_csv, root, season))
+        panel.add(tempButton)
     
     return panel
 
@@ -17,18 +34,25 @@ def load_tab_base(root):
 def generate_season_input_field(root):    
     season = tk.StringVar()
     
-    seasonInputLabel = tk.Label(root, text = "Enter the season you want to view:")
-    seasonInput = tk.Entry(root, text = "black", bg = "lightgrey", width = 15, textvariable = season)
-    seasonInputButton = tk.Button(root, width = 12, relief = RAISED, text = "Get season stats", command = load_season_stats)
+    panel = PanedWindow(root, bd = 1, relief = "raised", bg = "grey", orient = VERTICAL)
     
-    seasonInputLabel.pack(side = TOP, anchor = "w")
-    seasonInput.pack(side = TOP, anchor = "w")
-    seasonInputButton.pack(side = TOP, anchor = "w")
+    seasonInputLabel = tk.Label(panel, text = "Enter the season you want to view:")
+    seasonInputField = tk.Entry(panel, text = "black", bg = "lightgrey", width = 15, textvariable = season)
+    seasonInputButton = tk.Button(panel, width = 12, relief = RAISED, text = "Get season stats")
+    
+    seasonInputLabel.pack(side = TOP)
+    seasonInputField.pack(side = TOP)
+    seasonInputButton.pack(side = TOP)
+    
+    return panel
     
 # Generate table containing stats for selected season (Generated from stored CSV)
-def load_season_stats(root, season):    
-    print(season.get() + " Football season loaded")
-    margin = Frame(root)
+def display_csv(root, season):   
+    #panel = PanedWindow(root, bd = 1, relief = "sunken", bg = "grey", orient = VERTICAL) 
+    fileName = season + "footballplayerstats.csv"
+    print(fileName)
+    
+    margin = PanedWindow(root)
     scrollbarX = Scrollbar(margin, orient = HORIZONTAL)
     scrollbarY = Scrollbar(margin, orient = VERTICAL)
     tree = ttk.Treeview(margin, selectmode = "extended", yscrollcommand = scrollbarY.set, xscrollcommand = scrollbarX.set)
@@ -42,30 +66,4 @@ def load_season_stats(root, season):
     headTitles = list()
     playerValues = list()
     
-# Generate imput
-def generate_csv_input_field(root):    
-    season = tk.StringVar()
-    csvInputLabel = tk.Label(root, text = "Find the CSV you want to view:")
-    csvInput = tk.Entry(root, text = "black", bg = "lightgrey", width = 15, textvariable = season)
-    csvInputButton = tk.Button(root, width = 12, relief = RAISED, text = "Get CSV", command = load_csv)
-    
-    csvInputLabel.pack(side = TOP, anchor = "w")
-    csvInput.pack(side = TOP, anchor = "w")
-    csvInputButton.pack(side = TOP, anchor = "w")
-    
-# Load & visualize CSV
-def load_csv(root):    
-    print(localSeason + " Football season loaded")
-    margin = Frame(root)
-    scrollbarX = Scrollbar(margin, orient = HORIZONTAL)
-    scrollbarY = Scrollbar(margin, orient = VERTICAL)
-    tree = ttk.Treeview(margin, selectmode = "extended", yscrollcommand = scrollbarY.set, xscrollcommand = scrollbarX.set)
-    
-    scrollbarX.config(command = tree.xview)
-    scrollbarY.config(comman = tree.yview)
-    
-    scrollbarX.pack(side = BOTTOM, fill = X)
-    scrollbarY.pack(side = RIGHT, fill = Y)
-    
-    headTitles = list()
-    playerValues = list()
+    return
--- GUI/Stat-Ball-GUI.py
+++ GUI/Stat-Ball-GUI.py
@@ -6,25 +6,37 @@ import os
 import GUIhelper
 
 tkRoot = tk.Tk()
-tkRoot.geometry("1000x650")
+tkRoot.geometry("1200x800")
 tkRoot.resizable(width = True, height = True)
 tkRoot.title("Stat-Ball")
 
 tabControl = ttk.Notebook(tkRoot)
 
+# -- Generate Home tab
 homeTab = ttk.Frame(tabControl)
 homePanel = GUIhelper.load_tab_base(homeTab)
-GUIhelper.generate_csv_input_field(homePanel)
+# Add Home tab
+homePanel.pack(expand = True, fill = tk.BOTH)
 tabControl.add(homeTab, text = "Home")
 
+# -- Generate Football tab
 footballTab = ttk.Frame(tabControl)
 footballPanel = GUIhelper.load_tab_base(footballTab)
-GUIhelper.generate_season_input_field(footballPanel)
+# Generate CSV manager (left side)
+footballCSVPanel = GUIhelper.generate_csv_manager(footballPanel, "football")
+footballCSVPanel.add(GUIhelper.generate_season_input_field(footballCSVPanel))
+# Add CSV manager to panel
+footballPanel.add(footballCSVPanel)
+# Add CSV viewer to panel
+# Add Football tab
+footballPanel.pack(expand = True, fill = tk.BOTH)
 tabControl.add(footballTab, text = "Football")
 
+# -- Generate Basketball tab
 basketballTab = ttk.Frame(tabControl)
 basketballPanel = GUIhelper.load_tab_base(basketballTab)
-GUIhelper.generate_season_input_field(basketballPanel)
+# Add Basketball tab
+basketballPanel.pack(expand = True, fill = tk.BOTH)
 tabControl.add(basketballTab, text = "Basketball")
 
 tabControl.pack(expand = True, fill = tk.BOTH)
@@ -33,9 +45,3 @@ def season_entered():
     return
 
 tkRoot.mainloop()
-
-with open('CSVs/2022playerstats.csv') as f:
-        csvReader = csv.reader(f, delimiter = ',')
-        for row in csvReader:
-            if(csvReader.line_num < 1):
-                print(row)
