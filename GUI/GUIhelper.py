be01e388865f3f5a5819bc04dd66d962dfb3bcba
Dylan Drumm <dyl.drumm@gmail.com>
2022-12-19 14:49:32 -0500

    More work on GUI
    Added PanedWindow generator to GUIhelper to initialize tabs with resizeable/workable windows.  Other stuff too.

--- GUI/GUIhelper.py
+++ GUI/GUIhelper.py
@@ -1,17 +1,71 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from tkinter import filedialog
+import csv
 
-#Generate Label, Field and Button to enter season and load stats
-def generate_season_input_field(tab):
+localSeason = (str)
+
+# Generate default tab template
+def load_tab_base(root):
+    panel = PanedWindow(root, bd = 1, relief = "sunken", bg = "lightgrey")
+    panel.pack(expand = True, fill = tk.BOTH)
+    
+    return panel
+
+# Generate Label, Field and Button to enter season and load stats
+def generate_season_input_field(root):    
     season = tk.StringVar()
-    seasonInputLabel = tk.Label(tab, text = "Enter the season you want to view:")
-    seasonInput = tk.Entry(tab, fg = "white", bg = "gray", width = 12, textvariable = season)
-    seasonInputButton = tk.Button(tab, width = 12, relief = RAISED, text = "Get season stats", command = load_season_stats(seasonInput.get()))
     
-    seasonInputLabel.pack()
-    seasonInput.pack()
-    seasonInputButton.pack()
+    seasonInputLabel = tk.Label(root, text = "Enter the season you want to view:")
+    seasonInput = tk.Entry(root, text = "black", bg = "lightgrey", width = 15, textvariable = season)
+    seasonInputButton = tk.Button(root, width = 12, relief = RAISED, text = "Get season stats", command = load_season_stats)
     
-def load_season_stats():
-    tkSeasonRoot = tk.Tk()
+    seasonInputLabel.pack(side = TOP, anchor = "w")
+    seasonInput.pack(side = TOP, anchor = "w")
+    seasonInputButton.pack(side = TOP, anchor = "w")
+    
+# Generate table containing stats for selected season (Generated from stored CSV)
+def load_season_stats(root, season):    
+    print(season.get() + " Football season loaded")
+    margin = Frame(root)
+    scrollbarX = Scrollbar(margin, orient = HORIZONTAL)
+    scrollbarY = Scrollbar(margin, orient = VERTICAL)
+    tree = ttk.Treeview(margin, selectmode = "extended", yscrollcommand = scrollbarY.set, xscrollcommand = scrollbarX.set)
+    
+    scrollbarX.config(command = tree.xview)
+    scrollbarY.config(comman = tree.yview)
+    
+    scrollbarX.pack(side = BOTTOM, fill = X)
+    scrollbarY.pack(side = RIGHT, fill = Y)
+    
+    headTitles = list()
+    playerValues = list()
+    
+# Generate imput
+def generate_csv_input_field(root):    
+    season = tk.StringVar()
+    csvInputLabel = tk.Label(root, text = "Find the CSV you want to view:")
+    csvInput = tk.Entry(root, text = "black", bg = "lightgrey", width = 15, textvariable = season)
+    csvInputButton = tk.Button(root, width = 12, relief = RAISED, text = "Get CSV", command = load_csv)
+    
+    csvInputLabel.pack(side = TOP, anchor = "w")
+    csvInput.pack(side = TOP, anchor = "w")
+    csvInputButton.pack(side = TOP, anchor = "w")
+    
+# Load & visualize CSV
+def load_csv(root):    
+    print(localSeason + " Football season loaded")
+    margin = Frame(root)
+    scrollbarX = Scrollbar(margin, orient = HORIZONTAL)
+    scrollbarY = Scrollbar(margin, orient = VERTICAL)
+    tree = ttk.Treeview(margin, selectmode = "extended", yscrollcommand = scrollbarY.set, xscrollcommand = scrollbarX.set)
+    
+    scrollbarX.config(command = tree.xview)
+    scrollbarY.config(comman = tree.yview)
+    
+    scrollbarX.pack(side = BOTTOM, fill = X)
+    scrollbarY.pack(side = RIGHT, fill = Y)
+    
+    headTitles = list()
+    playerValues = list()
--- GUI/Stat-Ball-GUI.py
+++ GUI/Stat-Ball-GUI.py
@@ -13,19 +13,29 @@ tkRoot.title("Stat-Ball")
 tabControl = ttk.Notebook(tkRoot)
 
 homeTab = ttk.Frame(tabControl)
+homePanel = GUIhelper.load_tab_base(homeTab)
+GUIhelper.generate_csv_input_field(homePanel)
 tabControl.add(homeTab, text = "Home")
 
 footballTab = ttk.Frame(tabControl)
-GUIhelper.generate_season_input_field(footballTab)
+footballPanel = GUIhelper.load_tab_base(footballTab)
+GUIhelper.generate_season_input_field(footballPanel)
 tabControl.add(footballTab, text = "Football")
 
 basketballTab = ttk.Frame(tabControl)
-GUIhelper.generate_season_input_field(basketballTab)
+basketballPanel = GUIhelper.load_tab_base(basketballTab)
+GUIhelper.generate_season_input_field(basketballPanel)
 tabControl.add(basketballTab, text = "Basketball")
 
 tabControl.pack(expand = True, fill = tk.BOTH)
 
-tkRoot.mainloop()
-
 def season_entered():
     return
+
+tkRoot.mainloop()
+
+with open('CSVs/2022playerstats.csv') as f:
+        csvReader = csv.reader(f, delimiter = ',')
+        for row in csvReader:
+            if(csvReader.line_num < 1):
+                print(row)
